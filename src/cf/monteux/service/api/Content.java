c806cc15aa14fcc98df598fbf2ad737603b809ca
Rove Monteux <rmonteux@centrum.cz>
2017-08-07 14:41:15 +0200

    Content class for serving HTML.

--- src/cf/monteux/service/api/Content.java
+++ src/cf/monteux/service/api/Content.java
@@ -1,5 +1,29 @@
 package cf.monteux.service.api;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/content")
 public class Content {
 
+	private static final Logger log = Logger.getLogger(Content.class.getName());
+	
+	@GET
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_HTML)
+	public Response xmlToJson(String input) {
+		
+		log.log(Level.INFO, "Input: "+input);
+		
+		return Response.status(200).entity("<html><title>test</title><body>test</body></html>").build();
+		
+	}
 }
--- src/cf/monteux/service/api/package-info.java
+++ src/cf/monteux/service/api/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
- */
-/**
- * @author Rove Monteux
+ * Domain classes used to produce the RESTful services. 
  *
+ * @since 1.0
+ * @author Rove Monteux
+ * @version 1.0
  */
 package cf.monteux.service.api;
